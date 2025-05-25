8ae127a156e2667259ff12281dda45c20bedb616
Raam Lakshman <raama.ravuri@gmail.com>
2025-05-26 00:05:22 +0530

    ORM GitHub Action Test__3

--- cypress/e2e/LoginTest.cy.js
+++ cypress/e2e/LoginTest.cy.js
@@ -16,6 +16,25 @@
 
 
 
+// import ORM_Login from "./POM/ORM_Login.cy";
+
+// describe('OrangeHRM Login Test', () => {
+//   const login = new ORM_Login();
+
+//   it('should login successfully with valid credentials', () => {
+//     const username = Cypress.env('ORM_USERNAME');
+//     const password = Cypress.env('ORM_PASSWORD');
+
+//     // Optional check to catch issues early
+//     expect(username, 'Username should be defined').to.not.be.undefined;
+//     expect(password, 'Password should be defined').to.not.be.undefined;
+
+//     login.login(username, password);
+
+//     cy.url().should('include', '/dashboard'); // update as per your app
+//   });
+// });
+
 import ORM_Login from "./POM/ORM_Login.cy";
 
 describe('OrangeHRM Login Test', () => {
@@ -25,13 +44,12 @@ describe('OrangeHRM Login Test', () => {
     const username = Cypress.env('ORM_USERNAME');
     const password = Cypress.env('ORM_PASSWORD');
 
-    // Optional check to catch issues early
+    console.log('Username:', username); // Debug
     expect(username, 'Username should be defined').to.not.be.undefined;
     expect(password, 'Password should be defined').to.not.be.undefined;
 
     login.login(username, password);
 
-    cy.url().should('include', '/dashboard'); // update as per your app
+    cy.url().should('include', '/dashboard');
   });
 });
-
